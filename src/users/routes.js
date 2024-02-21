033ae3d38b4c62c8b48c915b4635d33b1c98f24d
Jack Mears <jack.mears@ymail.com>
2024-02-21 12:06:55 +0000

    enhancement: add controllers functions and routs

---
+++ src/users/routes.js
@@ -0,0 +1,18 @@
+const { Router } = require("express");
+const userRouter = Router();
+
+const { hashPass, comparePass } = require("../middleware/auth");
+
+const {
+  signupUser,
+  getAllUsers,
+  login,
+  deleteAllUsers,
+} = require("./controllers");
+
+userRouter.post("/users/signupUser", hashPass, signupUser);
+userRouter.post("/users/login", comparePass, login);
+userRouter.get("/users/getAllUsers", getAllUsers);
+userRouter.delete("/users/deleteAllUsers", deleteAllUsers);
+
+module.exports = userRouter;
